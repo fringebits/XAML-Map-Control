7e1be93afd149043a1267ccafba3be49bf45e553
Mike Gutmann <mikeg@fringebits.com>
2025-06-17 20:27:39 -0700

    Added new NOAA maps tile source.

---
+++ MapControl/Shared/NOAAMapsTileSource.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MapControl
+{
+    public class NOAAMapsTileSource : TileSource
+    {
+        public override Uri GetUri(int column, int row, int zoomLevel)
+        {
+            var uri = base.GetUri(column, row, zoomLevel - 2);
+
+            return uri;
+        }
+    }
+}
